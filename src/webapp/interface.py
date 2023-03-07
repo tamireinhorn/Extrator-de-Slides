5d8a3c0c9d88c89a29105e4cb5e0d95f19eb8445
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2023-03-07 07:17:41 -0300

    i hate myself

--- src/webapp/interface.py
+++ src/webapp/interface.py
@@ -1,22 +1,18 @@
 from flask import Flask, request, render_template, send_file
-import os
-from ExtratorSlides.SlideExtractor import video_processing, choose_video
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("home.html")
+
 
 @app.route("/upload", methods=["POST"])
-def upload():
-    try:
-        slide_length = int(request.form['slide_length'])
-        video_file = choose_video()
-        pdf_path = video_processing(video_file, slide_length)
-        return send_file(pdf_path, attachment_filename=os.path.basename(pdf_path))
-    except Exception as e:
-        return str(e)
+def process_video():
+    pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
